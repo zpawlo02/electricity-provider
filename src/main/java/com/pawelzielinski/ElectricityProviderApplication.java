ce6067f882d101c7d6235e3650b92805f0e66c87
pawel.zielinski <pawel.zielinski@comtronic.pl>
2022-05-17 12:07:27 +0200

    Initial commit

---
+++ src/main/java/com/pawelzielinski/ElectricityProviderApplication.java
@@ -0,0 +1,13 @@
+package com.pawelzielinski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricityProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricityProviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pawelzielinski/ElectricityProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawelzielinski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectricityProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
